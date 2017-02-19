dbcfbdcb233fd32c2f041f2af3e032c61be006f0
Rudys Natanael Acosta crousset <natanael926@gmail.com>
2017-02-18 23:03:59 -0400

    Metodo de agregar eventos y listar eventos, creado

--- app/app.js
+++ app/app.js
@@ -1,13 +1,46 @@
 var vm = new Vue({
 	el: "#events",
 	data: {
-
+		event: {},
+		events: []
 	},
 	mounted: function() {
-		console.log('Mounted');
+		this.fetchEvents();
 	},
-	
-	methods: function(){
 
+	methods: {
+
+		// We dedicate a method to retrieving and setting some data
+		fetchEvents: function() {
+			var events = [
+				{
+        			id: 1,
+        			name: 'TIFF',
+        			description: 'Toronto International Film Festival',
+        			date: '2015-09-10'
+      			},
+      			{
+        			id: 2,
+        			name: 'The Martian Premiere',
+        			description: 'The Martian comes to theatres.',
+        			date: '2015-10-02'
+      			},
+      			{
+        			id: 3,
+        			name: 'SXSW',
+        			description: 'Music, film and interactive festival in Austin, TX.',
+        			date: '2016-03-11'
+      			}
+			];
+			this.events = events;
+		},
+
+		// add the one event in the list events
+		addEvent: function() {
+			if(this.event.name.length > 0 && yhis.event.date.length > 0) {
+				this.events.push(this.event);
+				this.event = {};
+			} 
+		}
 	}
 });
